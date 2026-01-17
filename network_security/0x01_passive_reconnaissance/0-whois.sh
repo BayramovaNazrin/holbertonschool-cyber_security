bfe6fdffd86c097aa81cd8cd0800de12417a4a34
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-01-17 13:07:44 +0000

    .

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/ {v=substr($0, index($0, ":")+1); gsub(/^ /, "", v); gsub(/\r/, "", v); if ($1 ~ /Street/) v=v " "; if ($1 ~ /Ext/) $1=$1 ":"; print $1 "," v}' > "${1}.csv"
+whois "$1" | awk -F: '/^(Registrant|Admin|Tech)/ {v=substr($0, index($0, ":")+1); gsub(/\r/, "", v); if ($1 ~ /Street/) v=v " "; if ($1 ~ /Ext/) $1=$1 ":"; print $1 "," v}' > "${1}.csv"
