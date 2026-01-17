bf5a83009ff6689e3a3ca7d33fab531d8c69103f
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-01-17 15:27:26 +0000

    .

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent -oI -o "$1.txt" > /dev/null 2>&1
-cut -d ',' -f 1 "$1.txt"
+subfinder -d "$1" -silent -oI -o "$1.txt" > /dev/null 2>&1 && cut -d ',' -f 1 "$1.txt"
