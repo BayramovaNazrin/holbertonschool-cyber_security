d6b16aed138f2e908cb05be36b88f76a58580be6
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-02-18 04:38:12 -0500

    .

--- cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
+++ cybersecurity_basics/0x03_cryptography_basics/0-sha1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script hashes a password provided as an argument using SHA-1
-echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt
+echo -n "$1" | sha1sum | awk '{print $1}' > 0_hash.txt\n
