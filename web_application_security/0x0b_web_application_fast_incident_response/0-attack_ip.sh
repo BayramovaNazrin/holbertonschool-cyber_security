8f43cccb200984b1e9013857d2875621a36cb0aa
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-04-08 13:24:48 +0000

    .

--- web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
+++ web_application_security/0x0b_web_application_fast_incident_response/0-attack_ip.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-rep -Eo '^[0-9]{1,3}(\.[0-9]{1,3}){3}' logs.txt | sort | uniq -c
-rep -Eo '^[0-9]{1,3}(\.[0-9]{1,3}){3}' logs.txt | sort | uniq -c | tail -1
+rep -Eo '^[0-9]{1,3}(\.[0-9]{1,3}){3}' logs.txt | sort | uniq | tail -1
