5521d73624f404a42cf5bbc6d4e0db2c5d1e4b91
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-04-15 13:44:42 +0000

    .

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-grep "Accepted" auth.log \
+grep "Accepted" $1 \
 | grep -Eo 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' \
 | awk '{print $2}' \
 | grep -Ev '^(10\.|192\.168\.|172\.(1[6-9]|2[0-9]|3[0-1])\.)' \
