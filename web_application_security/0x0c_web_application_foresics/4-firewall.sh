f7b133e240962867b9b69e3c41da50d0fb738875
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-04-15 14:00:14 +0000

    .

--- web_application_security/0x0c_web_application_foresics/4-firewall.sh
+++ web_application_security/0x0c_web_application_foresics/4-firewall.sh
@@ -1,2 +1,13 @@
 #!/usr/bin
-grep -E "iptables|ufw" auth.log | grep -iE " -A | -I |allow|deny"
+# 1. Grep for the firewall commands
+# 2. Extract the part of the string after "allow " or "-dport "
+# 3. Use 'sort -u' to find unique ports
+# 4. Count them
+
+grep -E "ufw|iptables" auth.log | \
+    grep -iE "allow|ACCEPT" | \
+    awk -F'allow |dport ' '{print $2}' | \
+    awk '{print $1}' | \
+    sed 's/\/.*//' | \
+    sort -u | \
+    wc -l
