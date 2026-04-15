dd76e283327f102a65f5e9f30ee47953c4aee4b6
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-04-15 12:56:11 +0000

    .

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-grep -Eo '[0-9]{1,3}(\.[0-9]{1,3}){3}' auth.log | sort -u | wc -l 
+
+failed=$(grep "Failed password" auth.log | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
+accepted=$(grep "Accepted" auth.log | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | sort -u)
+
+comm -12 <(echo "$failed") <(echo "$accepted") | wc -l
