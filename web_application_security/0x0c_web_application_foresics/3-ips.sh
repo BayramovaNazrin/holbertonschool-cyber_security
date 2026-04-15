a124914b2dbef8d3a5bad1e1c9a701307d146dce
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-04-15 13:54:26 +0000

    .

--- web_application_security/0x0c_web_application_foresics/3-ips.sh
+++ web_application_security/0x0c_web_application_foresics/3-ips.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-grep "Accepted" $1 \
-| grep -Eo 'from ([0-9]{1,3}\.){3}[0-9]{1,3}' \
-| awk '{print $2}' \
-| grep -Ev '^(10\.|192\.168\.|172\.(1[6-9]|2[0-9]|3[0-1])\.)' \
-| sort -u \
-| wc -l
+grep "Accepted password for root" auth.log | awk '{print $11}' | sort -u | wc -l
