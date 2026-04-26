f314cc31dcba758d612b06b6964040da160a9ec1
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-04-26 14:05:43 +0000

    .

--- network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
+++ network_security/0x07_nmap_post_port_scan_scripting/0-nmap_default.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
-
-# Check if argument is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <target>"
-    exit 1
-fi
-
-TARGET="$1"
-
-# Run Nmap with default NSE scripts
-sudo nmap -sC "$TARGET"
+sudo nmap -sC "$1"
