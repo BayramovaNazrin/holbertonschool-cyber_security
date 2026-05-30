8960db1f28819a0aaa15e0c33a24fe942ae2eef6
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-05-30 16:41:02 +0000

    .

--- scripting_cyber/0x00-ruby_scripting/3-read_file.rb
+++ scripting_cyber/0x00-ruby_scripting/3-read_file.rb
@@ -2,15 +2,14 @@
 require 'json'
 
 def count_user_ids(path)
-
+  # 1. Read and parse the JSON file
   file_content = File.read(path)
-
-  
   data = JSON.parse(file_content)
 
-    
+  # 2. Extract all the userIds into an array, removing any nils
   user_ids = data.map { |item| item['userId'] }.compact
 
-  
+  # 3. Tally them up! This automatically counts occurrences of each ID
+  # and returns a Hash like: { 1 => 10, 2 => 8, ... }
   user_ids.tally
 end
