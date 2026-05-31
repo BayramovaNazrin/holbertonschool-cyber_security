4bc607e09fedbd05bb2cbffb6dd8ac87174052ef
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-05-31 06:39:55 +0000

    .

--- scripting_cyber/0x00-ruby_scripting/9-download_file.rb
+++ scripting_cyber/0x00-ruby_scripting/9-download_file.rb
@@ -13,12 +13,4 @@ def download_file(url, download_path)
     end
   end
 rescue StandardError => e
-  # Keep error quiet or standard depending on grader expectations
-end
-
-# Check for the exact number of arguments required
-if ARGV.length != 2
-  puts "Usage: 9-download_file.rb URL LOCAL_FILE_PATH"
-else
-  download_file(ARGV[0], ARGV[1])
 end
