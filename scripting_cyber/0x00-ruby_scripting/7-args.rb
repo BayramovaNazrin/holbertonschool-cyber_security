bb4d8cf6200639ef928fa3979f30d76e64a97cfb
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-05-31 06:28:55 +0000

    .

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -2,9 +2,11 @@
 
 def print_arguments
   puts "Arguments:"
-  ARGV.each do |arg|
-    puts arg
+  
+  ARGV.each_with_index do |arg, index|
+    puts "#{index + 1}. #{arg}"
   end
-  # Print exactly one empty trailing line
+  
+  # Print the final trailing empty line
   puts ""
 end
