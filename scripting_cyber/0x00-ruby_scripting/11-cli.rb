cc7d961b18feedaa620275eb2a2c5a8de14c8e72
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-05-31 07:19:32 +0000

    .

--- scripting_cyber/0x00-ruby_scripting/11-cli.rb
+++ scripting_cyber/0x00-ruby_scripting/11-cli.rb
@@ -1,10 +1,46 @@
 #!/usr/bin/env ruby
+
 require 'optparse'
 
-options = {}
-TASK_FILE = "tasks.txt"
+TASKS_FILE = 'tasks.txt'
 
-opt_parser = OptionParser.new do |opts|
+def add_task(task)
+  File.open(TASKS_FILE, 'a') { |f| f.puts(task) }
+  puts "Task '#{task}' added."
+end
+
+def list_tasks
+  if File.exist?(TASKS_FILE)
+    tasks = File.readlines(TASKS_FILE).map(&:chomp)
+    if tasks.empty?
+      puts "No tasks found."
+    else
+      tasks.each_with_index { |task, i| puts "#{i+1}. #{task}" }
+    end
+  else
+    puts "No tasks found."
+  end
+end
+
+def remove_task(index)
+  unless File.exist?(TASKS_FILE)
+    puts "No tasks file found."
+    return
+  end
+
+  tasks = File.readlines(TASKS_FILE).map(&:chomp)
+  if index < 1 || index > tasks.length
+    puts "Invalid index. Please provide a number between 1 and #{tasks.length}."
+    return
+  end
+
+  removed = tasks.delete_at(index - 1)
+  File.open(TASKS_FILE, 'w') { |f| f.puts(tasks) }
+  puts "Task '#{removed}' removed."
+end
+
+options = {}
+OptionParser.new do |opts|
   opts.banner = "Usage: cli.rb [options]"
 
   opts.on("-a", "--add TASK", "Add a new task") do |task|
@@ -15,40 +51,22 @@ opt_parser = OptionParser.new do |opts|
     options[:list] = true
   end
 
-  opts.on("-r", "--remove INDEX", "Remove a task by index") do |index|
-    options[:remove] = index.to_i
+  opts.on("-r", "--remove INDEX", Integer, "Remove a task by index") do |index|
+    options[:remove] = index
   end
 
   opts.on("-h", "--help", "Show help") do
     puts opts
     exit
   end
-end
+end.parse!
 
-begin
-  opt_parser.parse!(ARGV)
-rescue OptionParser::ParseError => e
-  puts e.message
-  exit 1
-end
-
-# Core logic
 if options[:add]
-  File.open(TASK_FILE, "a") { |f| f.puts(options[:add]) }
-elsif options[:remove]
-  if File.exist?(TASK_FILE)
-    tasks = File.readlines(TASK_FILE)
-    idx = options[:remove] - 1
-    if idx >= 0 && idx < tasks.length
-      tasks.delete_at(idx)
-      File.write(TASK_FILE, tasks.join)
-    end
-  end
+  add_task(options[:add])
 elsif options[:list]
-  puts "Tasks:"
-  if File.exist?(TASK_FILE)
-    File.readlines(TASK_FILE).each do |line|
-      puts line.strip
-    end
-  end
+  list_tasks
+elsif options[:remove]
+  remove_task(options[:remove])
+else
+  puts "No option provided. Use -h for help."
 end
