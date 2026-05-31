57f423e664502ae0f2c63959d463daea249b8a42
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-05-31 06:25:00 +0000

    .

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -3,16 +3,19 @@ require 'net/http'
 require 'uri'
 
 def get_request(url)
-  # Parse the URL string into a URI object
   uri = URI.parse(url)
-  
-  # Perform the HTTP GET request
   response = Net::HTTP.get_response(uri)
   
-  # Print the exact format required by the grader
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
-  puts response.body
+  
+  # If the body is just an empty JSON object, format it across two lines
+  if response.body == "{}"
+    puts "{"
+    puts "}"
+  else
+    puts response.body
+  end
 rescue StandardError => e
   puts "An error occurred: #{e.message}"
 end
