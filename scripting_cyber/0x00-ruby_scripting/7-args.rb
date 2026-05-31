2d7eb0f009684e8be39703c538110b0e1d7b652a
BayramovaNazrin <thisisnazrinbayramova@gmail.com>
2026-05-31 06:26:54 +0000

    .

--- scripting_cyber/0x00-ruby_scripting/7-args.rb
+++ scripting_cyber/0x00-ruby_scripting/7-args.rb
@@ -1,16 +1,12 @@
 #!/usr/bin/env ruby
 
 def print_arguments
-  # Check if any arguments were passed
-  if ARGV.empty?
-    puts "No arguments provided."
-  else
-    # Loop through each argument stored in ARGV and print it
-    ARGV.each do |arg|
-      puts arg
-    end
+  puts "Arguments:"
+  
+  ARGV.each do |arg|
+    puts arg
   end
+  
+  # Print an extra newline to match the desired trailing space
+  puts ""
 end
-
-# Call the method to execute when the script runs
-print_arguments
